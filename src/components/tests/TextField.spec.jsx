870952f7f685bc33ecb2d79ce0357ef1ff2b1364
jdy8739 <jdy8739@naver.com>
2024-11-27 20:53:32 +0900

    test: test TextField

--- src/components/tests/TextField.spec.jsx
+++ src/components/tests/TextField.spec.jsx
@@ -4,6 +4,10 @@ import React from 'react';
 import TextField from '@/components/TextField';
 import render from '@/utils/test/render';
 
-it('sample test.', async () => {
-  expect(1).toBe(1);
+it('The TextFiled has the classname which is injected as a prop.', async () => {
+  await render(<TextField className={'test-class'} />);
+
+  expect(screen.getByPlaceholderText('텍스트를 입력해 주세요.')).toHaveClass(
+    'test-class',
+  );
 });
