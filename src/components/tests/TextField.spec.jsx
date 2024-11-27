4c2e7b088ea4199606bff9c3205843c149e61226
jdy8739 <jdy8739@naver.com>
2024-11-27 21:52:24 +0900

    TextField test

--- src/components/tests/TextField.spec.jsx
+++ src/components/tests/TextField.spec.jsx
@@ -4,10 +4,28 @@ import React from 'react';
 import TextField from '@/components/TextField';
 import render from '@/utils/test/render';
 
-it('The TextFiled has the classname which is injected as a prop.', async () => {
-  await render(<TextField className={'test-class'} />);
+describe('These are TextFiled tests', () => {
+  test('The TextFiled has the classname which is injected as a prop.', async () => {
+    await render(<TextField className={'test-class'} />);
 
-  expect(screen.getByPlaceholderText('텍스트를 입력해 주세요.')).toHaveClass(
-    'test-class',
-  );
+    expect(screen.getByPlaceholderText('텍스트를 입력해 주세요.')).toHaveClass(
+      'test-class',
+    );
+  });
+
+  it('should be a TextField which has a placholder saying "텍스트를 입력해 주세요."', async () => {
+    await render(<TextField />);
+
+    const textField = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
+
+    expect(textField).toBeInTheDocument();
+  });
+
+  it('should change placeholder as injected placeholder prop"', async () => {
+    await render(<TextField placeholder={'상품명을 입력해 주세요.'} />);
+
+    const textField = screen.getByPlaceholderText('상품명을 입력해 주세요.');
+
+    expect(textField).toBeInTheDocument();
+  });
 });
