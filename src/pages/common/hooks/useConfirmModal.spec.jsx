0603ead97c39cc13870d25d617e5f990c341be87
jdy8739 <jdy8739@naver.com>
2024-11-28 15:55:21 +0900

    test custom hook

--- src/pages/common/hooks/useConfirmModal.spec.jsx
+++ src/pages/common/hooks/useConfirmModal.spec.jsx
@@ -1,9 +1,26 @@
 import { renderHook, act } from '@testing-library/react';
+import { test } from 'node_modules/vitest/dist/index';
 
 import useConfirmModal from './useConfirmModal';
 
-it('호출 시 initialValue 인자를 지정하지 않는 경우 isModalOpened 상태가 false로 설정된다.', () => {});
+test('호출 시 initialValue 인자를 지정하지 않는 경우 isModalOpened 상태가 false로 설정된다.', () => {
+  const { result } = renderHook(useConfirmModal);
 
-it('호출 시 initialValue 인자를 boolean 값으로 지정하는 경우 해당 값으로 isModalOpened 상태가 설정된다.', () => {});
+  expect(result.current.isModalOpened).toBe(false);
+});
 
-it('훅의 toggleIsModalOpened()를 호출하면 isModalOpened 상태가 toggle된다.', () => {});
+test('호출 시 initialValue 인자를 boolean 값으로 지정하는 경우 해당 값으로 isModalOpened 상태가 설정된다.', () => {
+  const { result } = renderHook(() => useConfirmModal(true));
+
+  expect(result.current.isModalOpened).toBe(true);
+});
+
+test('훅의 toggleIsModalOpened()를 호출하면 isModalOpened 상태가 toggle된다.', () => {
+  const { result } = renderHook(useConfirmModal);
+
+  act(() => {
+    result.current.toggleIsModalOpened();
+  });
+
+  expect(result.current.isModalOpened).toBe(true);
+});
