493bd7f932e2df461f9b3afffc20a986ed91be47
jdy8739 <jdy8739@naver.com>
2024-11-28 15:19:59 +0900

    test Module Mocking

--- src/pages/error/components/tests/ErrorPage.spec.jsx
+++ src/pages/error/components/tests/ErrorPage.spec.jsx
@@ -4,4 +4,20 @@ import React from 'react';
 import ErrorPage from '@/pages/error/components/ErrorPage';
 import render from '@/utils/test/render';
 
-it('"뒤로 이동" 버튼 클릭시 뒤로 이동하는 navigate(-1) 함수가 호출된다', async () => {});
+const spy = vi.fn();
+
+vi.mock('react-router-dom', async () => {
+  const original = await vi.importActual('react-router-dom');
+
+  return { ...original, useNavigate: () => spy };
+});
+
+it('should call useNavigate when users click "뒤로 가기" for just before the current page', async () => {
+  const { user } = await render(<ErrorPage />);
+
+  const button = await screen.getByRole('button', { name: '뒤로 이동' });
+
+  await user.click(button);
+
+  expect(spy).toHaveBeenNthCalledWith(1, -1);
+});
