f0fd55d1ef6f06e2242d4b39156586636484e096
jdy8739 <jdy8739@naver.com>
2024-11-28 10:51:07 +0900

    TextField test with spy function

--- src/components/tests/TextField.spec.jsx
+++ src/components/tests/TextField.spec.jsx
@@ -1,4 +1,5 @@
 import { screen } from '@testing-library/react';
+import { vi } from 'node_modules/vitest/dist/index';
 import React from 'react';
 
 import TextField from '@/components/TextField';
@@ -21,11 +22,47 @@ describe('These are TextFiled tests', () => {
     expect(textField).toBeInTheDocument();
   });
 
-  it('should change placeholder as injected placeholder prop"', async () => {
+  it('should change placeholder as injected placeholder prop.', async () => {
     await render(<TextField placeholder={'상품명을 입력해 주세요.'} />);
 
     const textField = screen.getByPlaceholderText('상품명을 입력해 주세요.');
 
     expect(textField).toBeInTheDocument();
+  });
+
+  it('should call the function injected as onChange prop when users type some text.', async () => {
+    const spy = vi.fn();
+
+    const { user } = await render(<TextField onChange={spy} />);
+
+    const textField = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
+
+    await user.type(textField, 'test');
+
+    expect(spy).toHaveBeenCalledWith('test');
+  });
+
+  it('should call then function injected as onEnter prop when user type Enter.', async () => {
+    const spy = vi.fn();
+
+    const { user } = await render(<TextField onChange={spy} />);
+
+    const textField = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
+
+    await user.type(textField, 'test{Enter}');
+
+    expect(spy).toHaveBeenCalledWith('test');
+  });
+
+  it('should call then function injected as onFocus prop when input blurred.', async () => {
+    const spy = vi.fn();
+
+    const { user } = await render(<TextField onFocus={spy} />);
+
+    const textField = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
+
+    await user.click(textField);
+
+    expect(spy).toHaveBeenCalledWith();
   });
 });
